27e3168ec0f7438f8334231111a69b235b1f5b2f
hasanhasibul <hasanhasibul395@gmail.com>
2023-06-05 00:21:04 +0600

    prettier check

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,10 +2,10 @@ import dotenv from 'dotenv'
 import path from 'path'
 
 dotenv.config({
-    path: path.join(process.cwd(), ".env")
+  path: path.join(process.cwd(), '.env'),
 })
 
 export default {
-    port: process.env.PORT,
-    dbUrl: process.env.DB_URL
+  port: process.env.PORT,
+  dbUrl: process.env.DB_URL,
 }
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,21 @@
-import mongoose from "mongoose"
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 // const port = 5000
 // const pass = "aWCdTuSxebcaBER2"
 // const user = "university-admin"
 // const URL = "mongodb+srv://university-admin:aWCdTuSxebcaBER2@cluster0.ezexp.mongodb.net/university-management?retryWrites=true&w=majority"
 
 const dbConnections = async () => {
-    try {
-        await mongoose.connect(config.dbUrl as string);
-        console.log('db connection success')
-        app.listen(config.port, () => {
-            console.log(`Application listening on port ${config.port}`)
-        })
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    await mongoose.connect(config.dbUrl as string)
+    console.log('db connection success')
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 dbConnections()
